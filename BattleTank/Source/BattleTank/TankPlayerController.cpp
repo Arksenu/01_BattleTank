51179728c6007830c65d9cce478d374f2a929065
arksenu <leizaro.fei@gmail.com>
2017-11-05 16:00:08 -0800

    Added possession check

--- BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -5,3 +5,22 @@
 
 
 
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	if (GetControlledTank())
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Possessing tank %s"), *GetControlledTank()->GetName());
+	}
+	else 
+	{
+		UE_LOG(LogTemp, Error, TEXT("NOT POSSESSING TANK"))
+	}
+
+}
+
+ATank* ATankPlayerController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
+
